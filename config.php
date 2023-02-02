c4804bec120e10b2da442140c0c74cf0bef88e74
Jose Jimmy <josejimmy8055@gmail.com>
2023-02-02 16:02:20 +0530

    config changed

--- config.php
+++ config.php
@@ -1,2 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'shop_db') or die('connection failed');
+
+$conn = mysqli_connect('localhost', 'root','', 'shop_db') or die('connection failed');
+
+?>
